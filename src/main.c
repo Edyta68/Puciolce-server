b255fdc22788238d26819fbff833c62f706855af
ytaa <trdo1009@gmail.com>
2018-09-05 23:25:09 -0700

    Deleted unnecessary comment.

--- src/main.c
+++ src/main.c
@@ -2,13 +2,12 @@
 
 int main(int argc, char** argv){
 
-  //printf("%lu", sizeof(struct RRC_ConnectionRequest));
-
   if(argc < 2){
     printf("Invalid number of arguments\n");
     exit(EXIT_FAILURE);
   }
 
   server_run(argc, argv);
+
   return 0;
 }
