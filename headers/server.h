baddf38c927f866f364b99c7fc063f95eb504f5f
ytaa <trdo1009@gmail.com>
2018-09-17 06:35:20 -0700

    Implemented reading of server options and passing them to server_run function.

--- headers/server.h
+++ headers/server.h
@@ -3,6 +3,7 @@
 #include "clients_handling.h"
 
 #define SERVER_LOGS_PING 1
+#define SERVER_ALREADY_EXISTING 2
 
 void server_run(unsigned short PORT, unsigned int options);
 void server_stop();
--- src/main.c
+++ src/main.c
@@ -2,12 +2,39 @@
 
 int main(int argc, char** argv){
 
-  if(argc < 2){
-    printf("Invalid number of arguments\n");
+  int optret = 0, server_options = 0;
+  opterr = 0;
+  char *existing_server_port;
+  while((optret = getopt(argc, argv, "lp:")) != -1){
+    switch (optret){
+      case 'l':
+        server_options |= SERVER_LOGS_PING;
+        break;
+      case 'p':
+        server_options |= SERVER_ALREADY_EXISTING;
+        existing_server_port = optarg;
+        break;
+      case '?':
+        if(optopt == 'p'){
+          fprintf(stderr, "Option -%c requiers an argument: existing servers port\n", optopt);
+        }
+        else{
+          fprintf(stderr, "Unknown option: -%c\n", optopt);
+        }
+        break;
+    }
+  }
+
+  if(optind >= argc){
+    printf("Invalid number of arguments.\n");
     exit(EXIT_FAILURE);
   }
 
-  server_run(atoi(argv[1]));
+  if(server_options & SERVER_ALREADY_EXISTING){
+    printf("Connecting to existing server on port: %s\n", existing_server_port);
+  }
+
+  server_run(atoi(argv[argc-1]), server_options);
 
   return 0;
 }
