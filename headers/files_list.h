a1e8baf7ff268a3898f23dd3c0f2ad6f678a6437
Ubuntu <przemyslawsnihur@gmail.com>
2018-09-25 06:30:09 -0700

    Added missing headers includes.

--- headers/files_list.h
+++ headers/files_list.h
@@ -1,6 +1,8 @@
 #include <dirent.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include "message_label.h"
 
 #define DIRECTORY_PATH "/home/przemeksnihur/test"
 #define SUCCESS_SEND_FILES_LIST 1
